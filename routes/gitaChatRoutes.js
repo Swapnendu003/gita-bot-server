1503f3de604269f85468020c4ba6b08b1e469e27
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2024-01-19 00:44:41 +0530

    Solved the Time out issue

--- routes/gitaChatRoutes.js
+++ routes/gitaChatRoutes.js
@@ -13,14 +13,14 @@ router.post('/generateResponse', async (req, res) => {
     const responseText = await chatController.generateResponse(userInput);
 
     // Split the response into chunks
-    const chunkSize = 512; // Adjust the chunk size as needed
+    const chunkSize = 1024; // Adjust the chunk size as needed
     const responseChunks = responseText.match(new RegExp(`.{1,${chunkSize}}`, 'g'));
 
     // Stream chunks with a delay
     async function streamChunks() {
       for (const chunk of responseChunks) {
         res.write(chunk);
-        await new Promise(resolve => setTimeout(resolve, 100)); // Adjust delay as needed
+        await new Promise(resolve => setTimeout(resolve, 200)); // Adjust delay as needed
       }
       res.end(); // End the response stream after all chunks are sent
     }
